62bd75a3f8ccf56f4e546c80da9eea4a3bc996f6
Aasrith-Lead <leadaasrith@gmail.com>
2025-09-25 12:08:47 +0530

    Delete week 7 directory

--- week 7/data.js
+++
@@ -1,6 +0,0 @@
-export const bookList = [
-  { title: "JS Basics", author: "Arjun ", price: 300, availability: "in stock" },
-  { title: "React Start", author: "Sanjay", price: 550, availability: "in stock" },
-  { title: "Node Tricks", author: "Rahul", price: 480, availability: "out of stock" },
-  { title: "Python Made Easy", author: "Ravi", price: 420, availability: "in stock" }
-];
